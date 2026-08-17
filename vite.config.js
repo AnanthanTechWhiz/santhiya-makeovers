f54bb7e268bc21485714684ed9c9633d2892e2e5
Ananthan P <anan.intech@gmail.com>
2026-08-17 21:22:42 +0530

    Fix Github pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // GitHub Pages serves the site from https://ananthantechwhiz.github.io/santhiya-makeovers/
+  // so all asset paths must be prefixed with the repo name.
+  base: '/santhiya-makeovers/',
   plugins: [react()],
 })
